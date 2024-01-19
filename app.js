4fc8dd766a3e9cec19f07c44a42217bd8f14487a
otteru <otteru@naver.com>
2024-01-19 07:51:39 +0000

    added day50-2 content

--- app.js
+++ app.js
@@ -1,34 +1,51 @@
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static("public")); // css, js file request solution
+app.use(express.urlencoded({extended: false}));
 
 app.get("/", function(req, res) {
 	const htmlFilePath = path.join(__dirname, "views", "index.html");
 	res.sendFile(htmlFilePath);
 })
 
-app.get('/restaurants', function(req, res) {
+app.get("/restaurants", function(req, res) {
 	const htmlFilePath = path.join(__dirname, "views", "restaurants.html");
 	res.sendFile(htmlFilePath);
 } )
 
-app.get('/about', function(req, res) {
+app.get("/about", function(req, res) {
 	const htmlFilePath = path.join(__dirname, "views", "about.html");
 	res.sendFile(htmlFilePath);
 } )
 
-app.get('/confirm', function(req, res) {
+app.get("/confirm", function(req, res) {
 	const htmlFilePath = path.join(__dirname, "views", "confirm.html");
 	res.sendFile(htmlFilePath);
 } )
 
-app.get('/recommend', function(req, res) {
+app.get("/recommend", function(req, res) {
 	const htmlFilePath = path.join(__dirname, "views", "recommend.html");
 	res.sendFile(htmlFilePath);
 } )
+
+app.post("/recommend", function(req, res) {
+	const restaurant = req.body; //object save
+	const filePath = path.join(__dirname, "data", "restaurants.json");
+	
+	const fileData = fs.readFileSync(filePath);
+	const storedRestaurants = JSON.parse(fileData);
+	
+	storedRestaurants.push(restaurant);
+	
+	fs.writeFileSync(filePath, JSON.stringify((storedRestaurants)));
+	
+	res.redirect("/confirm");
+	
+});
 
 app.listen(3000);
