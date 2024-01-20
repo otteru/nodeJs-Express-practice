40cb0673c759b0c45a00a212739ceae1c7cf4c0c
otteru <otteru@naver.com>
2024-01-20 11:55:39 +0000

    added day51 content

--- app.js
+++ app.js
@@ -5,32 +5,39 @@ const express = require("express");
 
 const app = express();
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 app.use(express.static("public")); // css, js file request solution
 app.use(express.urlencoded({extended: false}));
-
+   
 app.get("/", function(req, res) {
-	const htmlFilePath = path.join(__dirname, "views", "index.html");
-	res.sendFile(htmlFilePath);
+	// const htmlFilePath = path.join(__dirname, "views", "index.html");
+	// res.sendFile(htmlFilePath);
+	
+	res.render("index") //ejs ->  html
 })
 
 app.get("/restaurants", function(req, res) {
-	const htmlFilePath = path.join(__dirname, "views", "restaurants.html");
-	res.sendFile(htmlFilePath);
+	const filePath = path.join(__dirname, "data", "restaurants.json");
+	
+	const fileData = fs.readFileSync(filePath);
+	const storedRestaurants = JSON.parse(fileData);
+	
+	res.render("restaurants", { numberOfRestaurants: storedRestaurants.length, 
+							   restaurants : storedRestaurants})
 } )
 
 app.get("/about", function(req, res) {
-	const htmlFilePath = path.join(__dirname, "views", "about.html");
-	res.sendFile(htmlFilePath);
+	res.render("about");
 } )
 
 app.get("/confirm", function(req, res) {
-	const htmlFilePath = path.join(__dirname, "views", "confirm.html");
-	res.sendFile(htmlFilePath);
+	res.render("confirm");
 } )
 
 app.get("/recommend", function(req, res) {
-	const htmlFilePath = path.join(__dirname, "views", "recommend.html");
-	res.sendFile(htmlFilePath);
+	res.render("recommend");
 } )
 
 app.post("/recommend", function(req, res) {
