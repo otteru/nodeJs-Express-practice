03079c6ccc2a202b398892e6f668ce1bf452e2b3
otteru <otteru@naver.com>
2024-01-21 14:00:55 +0000

    added day52 content

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const path = require("path");
 
 const express = require("express");
+const uuid =require("uuid");
 
 const app = express();
 
@@ -16,9 +17,9 @@ app.get("/", function(req, res) {
 	// res.sendFile(htmlFilePath);
 	
 	res.render("index") //ejs ->  html
-})
+});
 
-app.get("/restaurants", function(req, res) {
+app.get("/restaurants", function(req, res) { 
 	const filePath = path.join(__dirname, "data", "restaurants.json");
 	
 	const fileData = fs.readFileSync(filePath);
@@ -26,22 +27,33 @@ app.get("/restaurants", function(req, res) {
 	
 	res.render("restaurants", { numberOfRestaurants: storedRestaurants.length, 
 							   restaurants : storedRestaurants})
-} )
+} );
 
-app.get("/about", function(req, res) {
-	res.render("about");
-} )
-
-app.get("/confirm", function(req, res) {
-	res.render("confirm");
-} )
+app.get("/restaurants/:id", function (req, res) {
+	const restaurantId = req.params.id;
+	const filePath = path.join(__dirname, "data", "restaurants.json");
+	
+	const fileData = fs.readFileSync(filePath);
+	const storedRestaurants = JSON.parse(fileData);
+	
+	for(const restaurant of storedRestaurants) {
+		if(restaurant.id== restaurantId) {
+			return res.render("restaurant-detail", {restaurant: restaurant});
+		}
+	}
+	
+	res.render('404');
+	
+});
 
 app.get("/recommend", function(req, res) {
 	res.render("recommend");
-} )
+} );
 
 app.post("/recommend", function(req, res) {
 	const restaurant = req.body; //object save
+	restaurant.id = uuid.v4();
+	
 	const filePath = path.join(__dirname, "data", "restaurants.json");
 	
 	const fileData = fs.readFileSync(filePath);
@@ -53,6 +65,22 @@ app.post("/recommend", function(req, res) {
 	
 	res.redirect("/confirm");
 	
+});
+
+app.get("/confirm", function(req, res) {
+	res.render("confirm");
+} );
+
+app.get("/about", function(req, res) {
+	res.render("about");
+} );
+
+app.use(function(req,res) {
+	res.render("404");
+});
+
+app.use(function(error, req, res, next) {
+	res.render("500");
 });
 
 app.listen(3000);
